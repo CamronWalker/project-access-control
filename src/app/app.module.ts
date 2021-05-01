6faed58a6b80a3d6227e707f508f15dc3e377d62
Camron Walker <camronwalker@gmail.com>
2021-04-30 22:49:58 -0600

    cont setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCqnTqiZNg1pzcoMrCSU2Su2jUngmZfWxQ",
+    authDomain: "project-access-control.firebaseapp.com",
+    projectId: "project-access-control",
+    storageBucket: "project-access-control.appspot.com",
+    messagingSenderId: "665528508442",
+    appId: "1:665528508442:web:ce01348e3faddcb75cb1a3",
+    measurementId: "G-30EY07CG8Q"
+  }
 };
 
 /*
